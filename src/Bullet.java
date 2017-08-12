aecf6a2509670cff46a247477bd88b66ff76e7f4
AlexV <alex.v.adamov@gmail.com>
2017-08-12 16:53:05 +0300

    Added bullets (on Mouse1)

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,3 +1,30 @@
-public class Bullet {
+import java.awt.*;
 
+public class Bullet {
+    //Fields
+    private double x;
+    private double y;
+    private int r;
+    private Color color;
+
+    private int speed;
+
+    //Constructor
+    public Bullet() {
+        this.x = GamePanel.player.getX();
+        this.y = GamePanel.player.getY();
+        this.r = 2;
+        this.color = Color.BLACK;
+        this.speed = 6;
+    }
+
+    //Functions
+    public void update(){
+        y -=speed;
+    }
+
+    public void draw(Graphics2D g){
+        g.setColor(color);
+        g.fillOval((int)x,(int)y, r, 2*r);
+    }
 }
